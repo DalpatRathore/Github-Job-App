9fab6921c70b8ca2cd357dd502a20fcfb0f99293
ErDalpatRathore <er.dalpat.rathore@gmail.com>
2021-03-18 11:26:25 +0530

    Final Commit of Web-Dev-Simplified Tutorial Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import "./App.css";
+import React, { useState } from "react";
+import useFetchJobs from "./useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Job";
+import JobsPagination from "./JobsPagination";
+import SearchForm from "./SearchForm";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value };
+    });
+  }
   return (
-    <div className="App">
-      <h1>Github Job Application</h1>
-    </div>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm
+        params={params}
+        onParamChange={handleParamChange}
+      ></SearchForm>
+
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      ></JobsPagination>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      ></JobsPagination>
+    </Container>
   );
 }
 
